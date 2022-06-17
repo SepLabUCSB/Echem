929115f71a541581d51924c5de68723f51a23779
bwroehrich <amdowns@ucsb.edu>
2022-06-17 15:45:41 -0700

    us precision for saved starting time

--- EIS_Control.py
+++ EIS_Control.py
@@ -1472,7 +1472,7 @@ class MainWindow:
                 with open(meta_file, 'w') as f:
                     f.write('Waveform Vpp (mV): '+ str(self.waveform_vpp.get('1.0', 'end')))
                     f.write('Waveform: '+ str(self.waveform.get()))
-                    s_t = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time()))
+                    s_t = datetime.now().strftime("%H:%M:%S.%f")
                     f.write('\nStart time: %s'%s_t)
                     avg_Vpp = np.mean([self.ft[frame].Vpp for frame in self.ft])
                     f.write(f'\nExperimental Vpp (V): {avg_Vpp}')
