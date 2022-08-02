349c5f2f95619fd9315c8b398c7b14b209d2ad04
bwroehrich <amdowns@ucsb.edu>
2022-08-02 09:10:37 -0700

    move log to Recorder class

--- EIS_Control.py
+++ EIS_Control.py
@@ -36,7 +36,6 @@ colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
 '''
 To add:
 
-Plot multiple Z vs t for multiplexed in-vivo
 
 '''
 
@@ -48,13 +47,13 @@ LOGGING = True
 
 log_file = os.path.expanduser('~\Desktop\EIS Output\log.txt')
 
-def log(text):
-    global log_file
-    if text != '\n' and text != '' and LOGGING:
-        with open(log_file, 'a') as f:
-            t = str(datetime.now().time())
-            f.write(t + '\t' + text + '\n')
-            f.close()
+#def log(text):
+#    global log_file
+#    if text != '\n' and text != '' and LOGGING:
+#        with open(log_file, 'a') as f:
+#            t = str(datetime.now().time())
+#            f.write(t + '\t' + text + '\n')
+#            f.close()
 
             
             
@@ -67,6 +66,7 @@ class PrintLogger():
         self.textbox = textbox # keep ref
 
     def write(self, text):
+        # use Recorder.log() to log console output
         self.Rec.log(text)
         self.textbox.insert(tk.END, text) # write text to textbox
         self.textbox.see('end') # scroll to end
